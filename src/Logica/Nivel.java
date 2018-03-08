3abaf0ab8d7d0f439c88933b0e797e76ebb0faac
sanchezbuitrago <sanchezbuitrago@hotmail.com>
2018-03-08 11:37:42 -0500

    Se elimina la jerarquia niveles, dejando solo una clase nivel, se agrega la funcionalidad de leer todos los niveles y de pasar nivel.

--- src/Logica/Nivel.java
+++ src/Logica/Nivel.java
@@ -1,32 +1,51 @@
 package Logica;
 
-
+import Serializable.DatosNivel;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 /**
  * @author ASUS
  * @version 1.0
  * @created 27-feb.-2018 11:11:24 a.m.
  */
-public abstract class Nivel {
+public class Nivel1 extends Nivel {
+    DatosNivel nivel = null;
+    public Nivel1() {
+        try {
+            FileInputStream fileIn = new FileInputStream("./niveles/fuck.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            nivel = (DatosNivel) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Datos nivel class not found");
+            c.printStackTrace();
+            return;
+        }
+        System.out.println(nivel.getColumnas());
+    }
 
-	private int x;
-	private int y;
+    @Override
+    public void construir(Tablero tablero, Personaje personaje) {
+        ubicarpersonaje(personaje);
 
+        tablero.setFilas(nivel.getFilas());
+        tablero.setColumnas(nivel.getColumnas());
+        tablero.setMatriz(nivel.getMatriz());
+        tablero.apagarluces();
+    }
 
-
-	public void finalize() throws Throwable {
-
-	}
-
-	public Nivel(){
-
-	}
-
-	/**
-	 * 
-	 * @param tablero    tablero
-	 * @param personaje
-	 */
-	public abstract void construir(Tablero tablero, Personaje personaje);
+    public void ubicarpersonaje(Personaje personaje)
+    {
+        personaje.setColumna(0);
+        personaje.setFila(0);
+        personaje.setOrientacion(2);
+    }
 
 }
+
